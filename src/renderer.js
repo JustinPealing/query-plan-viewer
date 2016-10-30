f6168ef58d493b690e061164572f83f516ebdfb8
Justin Pealing <Justin Pealing>
2016-10-30 12:19:31 +0000

    Show query plan.

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,9 @@
 const {ipcRenderer} = require('electron');
+const fs = require('fs');
 
 ipcRenderer.on('open-file', (event, arg) => {
-    
+    fs.readFile(arg, function(err, contents) {
+        const container = document.getElementById('container');
+        QP.showPlan(container, contents);
+    });
 });
